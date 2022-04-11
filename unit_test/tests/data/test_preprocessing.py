01362f27ba63f082a0d3dc52f9a573a53d2a0b87
Dinara Issagaliyeva <dinarissaa@gmail.com>
2022-04-11 16:55:04 +0400

    Add unit test for preprocessing.py

--- unit_test/tests/data/test_preprocessing.py
+++ unit_test/tests/data/test_preprocessing.py
@@ -1,5 +1,5 @@
 import pytest
-from unit_test.data.preprocessing import row_to_list
+from unit_test.data.preprocessing import row_to_list, convert_to_int
 
 
 def test_for_clean_row():
@@ -7,8 +7,18 @@ def test_for_clean_row():
 
 
 def test_for_missing_area():
-    assert row_to_list('\t293,410\n') is None
+    actual = row_to_list('\t293,410\n')
+    expected = None
+    message = ('row_to_list("\t293,410\n") returned'
+               '{0} instead of {1}'.format(actual, expected))
+    assert actual is expected, message
 
 
 def test_for_missing_tab():
     assert row_to_list('1,41234,323\n') is None
+
+
+def test_on_string_with_one_comma():
+    value = convert_to_int('2,081')
+    assert isinstance(value, int), f'The value {value} has to be int'
+
